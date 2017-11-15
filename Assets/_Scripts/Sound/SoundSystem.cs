01984655fee9b32a16bf9910a110e3afc03d3196
David Passlow <darklight437@gmail.com>
2017-11-15 13:00:34 +1100

    sounds basically setup

--- Assets/_Scripts/Sound/SoundSystem.cs
+++ Assets/_Scripts/Sound/SoundSystem.cs
@@ -3,14 +3,33 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class SoundSystem : MonoBehaviour {
+    //each type of high level audio file
+    public AudioSource idleAwake;
+    public AudioSource walk;
+    public AudioSource EndWalk;
+    public AudioSource attack;
+    public AudioSource takeDamage;
+    public AudioSource TilePlace;
+    public AudioSource TileAction;
+
 
 	// Use this for initialization
-	void Start () {
-		
-	}
+	void Start ()
+    {
+        playsound(idleAwake);
+    }
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void Update ()
+    {
+        
 	}
+
+
+    //plays the param sound
+    public void playsound(AudioSource Sound)
+    {
+        Sound.Play();
+       
+    }
 }
